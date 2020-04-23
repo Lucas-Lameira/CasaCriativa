a0be770a523e5c6fa7bf636376afe87bfbfd99f5
Lucas de souza lameira <lucasslameira@gmail.com>
2020-04-23 12:19:46 -0300

    semifinal
    nunjucks
    dados no backend pro front

--- server.js
+++ server.js
@@ -2,16 +2,70 @@
 const express = require("express")
 const server = express()
 
+
+//config nunjucks
+//viwes é o nome da pasta q contém os html
+const nunjucks = require("nunjucks")
+nunjucks.configure("views", {
+    express:server,
+    noCache: true, //ddesabilitar o cache no desenvolvimento
+})
+
 //config de arquivos estaticos(css, js, image)
 server.use(express.static("public"))
 
-//rota
+
+//colecoes de ideias 
+const ideas = [
+    {
+        img: "https://image.flaticon.com/icons/svg/2463/2463510.svg",
+        title:"Cursos de programação",
+        category:"Estudo",
+        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam soluta beatae labore accusamus pariatur. Nobis adipisci, ullam laborum ducimus aspernatur consequuntur non.",
+        url: "https://github.com/Lucas-Lameira"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2784/2784620.svg",
+        title:"Música",
+        category:"Lazer",
+        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam soluta beatae labore accusamus pariatur. Nobis adipisci, ullam laborum ducimus aspernatur consequuntur non.",
+        url:"https://github.com/Lucas-Lameira"
+    },
+    {
+        img:"https://image.flaticon.com/icons/svg/2780/2780096.svg",
+        title:"Exercicios",
+        category:"Saúde",
+        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam soluta beatae labore accusamus pariatur. Nobis adipisci, ullam laborum ducimus aspernatur consequuntur non.",
+        url:"https://github.com/Lucas-Lameira"
+    },
+    {
+        img:"https://www.flaticon.com/premium-icon/icons/svg/2790/2790316.svg",
+        title:"Games",
+        category:"Lazer",
+        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam soluta beatae labore accusamus pariatur. Nobis adipisci, ullam laborum ducimus aspernatur consequuntur non.",
+        url:"https://github.com/Lucas-Lameira"
+    }
+]
+
+
+
+//rotas
 server.get("/", function(request , response){
-    return response.sendFile(__dirname + "/index.html")
+
+    let frontIdea = []
+    for (let idea of ideas){
+        if(frontIdea.length < 3){
+            frontIdea.push(idea)
+        }
+    }
+
+    return response.render("index.html", {ideas: frontIdea} )//mandar o array ideas para a home 
 })
 
 server.get("/ideias", function(request , response){
-    return response.sendFile(__dirname + "/ideias.html")
+
+
+    return response.render("ideias.html", {ideas})
 })
 
 //Servidor na porta 3000
